c7278490fc7c2b0be76c4deeba7a9df0a43ec866
unclejimao <iyongyong@126.com>
2019-05-14 14:46:46 +0800

    windons forms SayHello instance

---
+++ Docs.MicroSoft/WindowsFormsSayHello/WindowsFormsSayHello/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsFormsSayHello
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SayHelloButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // SayHelloButton
+            // 
+            this.SayHelloButton.Font = new System.Drawing.Font("微软雅黑", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.SayHelloButton.Location = new System.Drawing.Point(323, 235);
+            this.SayHelloButton.Name = "SayHelloButton";
+            this.SayHelloButton.Size = new System.Drawing.Size(180, 60);
+            this.SayHelloButton.TabIndex = 0;
+            this.SayHelloButton.Text = "打招呼";
+            this.SayHelloButton.UseVisualStyleBackColor = true;
+            this.SayHelloButton.Click += new System.EventHandler(this.SayHelloButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(249, 109);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(360, 53);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Hello there!";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.SayHelloButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SayHelloButton;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Docs.MicroSoft/WindowsFormsSayHello/WindowsFormsSayHello/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsSayHello
+{
+    public partial class Form1 : Form
+    {
+        Random rand;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            rand = new Random();
+
+        }
+
+        private byte[] GetRandomBytes(int n)
+        {
+            var randomBytes = new byte[n];
+
+            rand.NextBytes(randomBytes);
+
+            return randomBytes;
+        }
+
+        private void SayHelloButton_Click(object sender, EventArgs e)
+        {
+            byte[] rgb = GetRandomBytes(3);
+
+            textBox1.ForeColor = Color.FromArgb(255, rgb[0], rgb[1], rgb[2]);
+            SayHelloButton.ForeColor = Color.FromArgb(255, rgb[0], rgb[1], rgb[2]);
+        }
+    }
+}
