8f74e2c409aa493d564c345e82793ce2fd6913d8
unclejimao <iyongyong@126.com>
2019-05-14 14:30:03 +0800

    docs.microsoft.com编写WPF应用案例

---
+++ Docs.MicroSoft/WpfSayHello/WpfSayHello/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSayHello
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rand;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            rand = new Random();
+
+        }
+
+        private byte[] GetRandomBytes(int n)
+        {
+            var randomBytes = new byte[n];
+            rand.NextBytes(randomBytes);
+            return randomBytes;
+        }
+
+        private void SayHelloButton_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] rgb = GetRandomBytes(3);
+
+            var randomColorBrush = new SolidColorBrush(Color.FromArgb(255, rgb[0], rgb[1], rgb[2]));
+
+            textBox1.BorderBrush = randomColorBrush;
+            textBox1.Foreground = randomColorBrush;
+
+            SayHelloButton.Foreground = randomColorBrush;
+            SayHelloButton.BorderBrush = randomColorBrush;
+        }
+    }
+}
