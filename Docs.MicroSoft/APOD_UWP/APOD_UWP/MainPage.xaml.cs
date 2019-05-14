b7ceb760c1018e344a229478eba393133ddedfee
unclejimao <iyongyong@126.com>
2019-05-14 19:08:13 +0800

    图像处理代码

--- Docs.MicroSoft/APOD_UWP/APOD_UWP/MainPage.xaml.cs
+++ Docs.MicroSoft/APOD_UWP/APOD_UWP/MainPage.xaml.cs
@@ -27,7 +27,7 @@ namespace APOD_UWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        const string EndpointURl = "https://api.nasa.gov/planetary/apod";
+        const string EndpointURL = "https://api.nasa.gov/planetary/apod";
 
         DateTime launchDate = new DateTime(1995, 6, 16);
 
@@ -59,9 +59,95 @@ namespace APOD_UWP
             MonthCalendar.MinDate = launchDate;
         }
 
-        private void MonthCalendar_DateChanged(CalendarDatePicker sender, CalendarDatePickerDateChangedEventArgs args)
+        private async void MonthCalendar_DateChangedAsync(CalendarDatePicker sender, CalendarDatePickerDateChangedEventArgs args)
         {
+            await RetrievePhoto();
+        }
 
+        private bool IsSupportedFormat(string photoURL)
+        {
+            var ext = Path.GetExtension(photoURL).ToLower();
+
+            return (ext == ".jpg" || ext == ".jpeg" || ext == ".png" || ext == ".gif" ||
+                    ext == ".tif" || ext == ".bmp" || ext == ".ico" || ext == ".svg");
+        }
+
+        private async Task RetrievePhoto()
+        {
+            var client = new HttpClient();
+            JObject jResult = null;
+            string responseContent = null;
+            string description = null;
+            string photoUrl = null;
+            string copyright = null;
+
+            // Set the UI elements to defaults
+            ImageCopyrightTextBox.Text = "NASA";
+            DescriptionTextBox.Text = "";
+
+            // Build the date parameter string for the date selected, or the last date if a range is specified.
+            DateTimeOffset dt = (DateTimeOffset)MonthCalendar.Date;
+
+            string dateSelected = $"{dt.Year.ToString()}-{dt.Month.ToString("00")}-{dt.Day.ToString("00")}";
+            string URLParams = $"?date={dateSelected}&api_key=DEMO_KEY";
+
+            // Populate the HTTP client appropriately.
+            client.BaseAddress = new Uri(EndpointURL);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // The critical call: send a GET request with the appropriate parameters.
+            HttpResponseMessage response = client.GetAsync(URLParams).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                // Be ready to catch any data or server errors.
+                try
+                {
+                    // Parse the response by using the Newtonsoft APIs.
+                    responseContent = await response.Content.ReadAsStringAsync();
+
+                    // Parse the response string for the details we need.
+                    jResult = JObject.Parse(responseContent);
+
+                    // Now get the image.
+                    photoUrl = (string)jResult["url"];
+                    var photoURI = new Uri(photoUrl);
+                    var bmi = new BitmapImage(photoURI);
+
+                    ImagePictureBox.Source = bmi;
+
+                    if (IsSupportedFormat(photoUrl))
+                    {
+                        // Get the copyright message, but fill with "NASA" if no name is provided.
+                        copyright = (string)jResult["copyright"];
+                        if (copyright != null && copyright.Length > 0)
+                        {
+                            ImageCopyrightTextBox.Text = copyright;
+                        }
+
+                        // Populate the description text box.
+                        description = (string)jResult["explanation"];
+                        DescriptionTextBox.Text = description;
+                    }
+                    else
+                    {
+                        DescriptionTextBox.Text = $"Image type is not supported. URL is {photoUrl}";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    DescriptionTextBox.Text = $"Image data is not supported. {ex.Message}";
+                }
+
+                // Keep track of our downloads in case we reach the limit.
+                ++imageCountToday;
+                ImagesTodayTextBox.Text = imageCountToday.ToString();
+            }
+            else
+            {
+                DescriptionTextBox.Text = "We were unable to retrieve the NASA picture for that day: " +
+                    $"{response.StatusCode.ToString()} {response.ReasonPhrase}";
+            }
         }
     }
 }
