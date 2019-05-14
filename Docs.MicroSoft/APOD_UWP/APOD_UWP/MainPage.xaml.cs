52a6e286ddfbbe496a1172f9f5047f81d47a2741
unclejimao <iyongyong@126.com>
2019-05-14 16:32:06 +0800

    创建项目并完成UI搭建

---
+++ Docs.MicroSoft/APOD_UWP/APOD_UWP/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace APOD_UWP
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        const string EndpointURl = "https://api.nasa.gov/planetary/apod";
+
+        DateTime launchDate = new DateTime(1995, 6, 16);
+
+        int imageCountToday;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            MonthCalendar.MinDate = launchDate;
+            MonthCalendar.MaxDate = DateTime.Today;
+        }
+
+        private void LaunchButton_Click(object sender, RoutedEventArgs e)
+        {
+            LimitRangeCheckBox.IsChecked = false;
+
+            MonthCalendar.DataContext = launchDate;
+        }
+
+        private void LimitRangeCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            var firstDayOfThisYear = new DateTime(DateTime.Today.Year, 1, 1);
+            MonthCalendar.MinDate = firstDayOfThisYear;
+        }
+
+        private void LimitRangeCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            MonthCalendar.MinDate = launchDate;
+        }
+
+        private void MonthCalendar_DateChanged(CalendarDatePicker sender, CalendarDatePickerDateChangedEventArgs args)
+        {
+
+        }
+    }
+}
